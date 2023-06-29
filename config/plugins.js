7b9ce2b23b159e463499c7e80dd1472954c8b64d
Isaac Anohene <anoheneisaac@gmail.com>
2023-06-29 21:32:40 +0000

    add import export plugin

--- config/plugins.js
+++ config/plugins.js
@@ -14,5 +14,11 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  'import-export-entries': {
+    enabled: true,
+    config: {
+      // See `Config` section.
+    },
+  },
   // ...
 });
